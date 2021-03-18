5fd57e758d110bf6a31b8487a80f491596be9342
Hbinbin <565703025@qq.com>
2021-03-18 10:36:41 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -68,11 +68,9 @@ export const deepClone = obj => {
   Object.keys(clone).forEach(
     key => (clone[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
   )
-  return Array.isArray(obj) && obj.length
+  return Array.isArray(obj)
     ? (clone.length = obj.length) && Array.from(clone)
-    : Array.isArray(obj)
-      ? Array.from(obj)
-      : clone
+    : clone
 }
 /**
  * 防抖
