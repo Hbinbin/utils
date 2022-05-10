1c63f7847ef1aa456bb9d8997daf9a2e736c49d5
haobin <haobin_web@sina.com>
2022-05-10 12:00:57 +0800

    feat: FP

--- utils/utils.common.js
+++ utils/utils.common.js
@@ -287,7 +287,25 @@ export const runAsync = fn => {
     };
   });
 };
-
+/**
+ * 函数式编程
+ * 1. 纯函数
+ * 2. 函数柯里华
+ * 3. 函数组合
+ * 4. Point Free
+ * https://llh911001.gitbooks.io/mostly-adequate-guide-chinese/content/ch5.html
+ */
+// 函数组合
+// 传入多个功能函数
+const compose = (...fns) => {
+  // 传入待处理的参数
+  return x => {
+    // 从右向左用传入的功能函数处理参数
+    return fns.reduceRight((value, fn) => {
+      return fn(value)
+    }, x)
+  }
+}
 /**
  * 生成长度为11的随机字母数字字符串
  */
